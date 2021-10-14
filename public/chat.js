85fbbef7daf2a7ef0133c1752653b6b5201168eb
Iván L. Murillo <diskman199@gmail.com>
2021-10-13 21:02:45 -0500

    Broadcasting

--- public/chat.js
+++ public/chat.js
@@ -7,8 +7,13 @@ let message = document.getElementById('message'),
     handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
     output = document.getElementById('output');
+    feedback = document.getElementById('feedback');
 
 handle.value = sessionStorage.getItem('handle') || '';
+
+message.addEventListener('keypress', () =>{
+    socket.emit('typing', handle.value);
+})
 
 // Emit events
 btn.addEventListener('click', () => {
@@ -24,5 +29,10 @@ btn.addEventListener('click', () => {
 
 // Listen for events
 socket.on('chat', data => {
+    feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', data =>{
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
